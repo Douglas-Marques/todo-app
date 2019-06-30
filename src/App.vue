cfa16e14bd105a939a7482dd9b7c671e0fdd2bf0
Douglas Marques <douglas.marques@dbccompany.com.br>
2019-06-30 02:14:03 -0300

    a lot

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,15 @@
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
+import Navbar from "@/components/Navbar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar
   },
-  data () {
-    return {
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
